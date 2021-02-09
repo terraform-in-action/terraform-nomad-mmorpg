104d09f189a64cc19a0ad5ffe8f07b8702d27b5f
Scott Winkler <Scott.Winkler@elliemae.com>
2021-02-08 21:48:56 -0800

    update

--- main.tf
+++ main.tf
@@ -11,26 +11,26 @@ data "nomad_regions" "current" {
 }
 
 locals {
-  aws_region = sort(data.nomad_regions.current.regions)[1]
+  aws_region   = sort(data.nomad_regions.current.regions)[1]
   azure_region = sort(data.nomad_regions.current.regions)[0]
 }
 
 resource "nomad_job" "aws_fabio" {
-  jobspec = templatefile("${path.module}/jobs/aws_fabio.hcl",{region = local.aws_region})
+  jobspec  = templatefile("${path.module}/jobs/aws_fabio.hcl", { region = local.aws_region })
   provider = nomad.aws
 }
 
 resource "nomad_job" "azure_fabio" {
-  jobspec = templatefile("${path.module}/jobs/azure_fabio.hcl",{region = local.azure_region})
+  jobspec  = templatefile("${path.module}/jobs/azure_fabio.hcl", { region = local.azure_region })
   provider = nomad.azure
 }
 
 resource "nomad_job" "azure_mongo" {
-  jobspec = templatefile("${path.module}/jobs/mongo.hcl",{region = local.azure_region})
+  jobspec  = templatefile("${path.module}/jobs/mongo.hcl", { region = local.azure_region })
   provider = nomad.azure
 }
 
 resource "nomad_job" "aws_browserquest" {
-  jobspec = templatefile("${path.module}/jobs/browserquest.hcl",{region=local.aws_region,address=replace(var.fabio_db,"tcp://","")})
+  jobspec  = templatefile("${path.module}/jobs/browserquest.hcl", { region = local.aws_region, address = replace(var.fabio_db, "tcp://", "") })
   provider = nomad.aws
 }
